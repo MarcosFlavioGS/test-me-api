01d46c74218ad5bb30c872564567e7ecfbd715b2
Marcos Flavio <mfghost69@gmail.com>
2024-09-22 15:40:29 -0300

    Feat: add config module and create OpenIA module

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { AppController } from '@/app.controller'
+import { AppService } from '@/app.service'
+
+import { isProduction } from '@/helper/environment'
+import { OpenIaModule } from './module/open-ia/open-ia.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      ignoreEnvFile: isProduction
+    }),
+    OpenIaModule
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
---
+++ app/src/helper/environment.ts
@@ -0,0 +1,3 @@
+export const isProduction: boolean = process.env.NODE_ENV === 'production'
+
+export const isDevelopment: boolean = process.env.NODE_ENV === 'development'
